49f4d2de70819b945aa561725a3fcb85e85b847b
nglinh <nglinh2312@gmail.com>
2013-09-10 22:18:43 +0800

    Added Movie view and Movie model Added dummy movie collection

--- js/zope.js
+++ js/zope.js
@@ -1,23 +1,23 @@
-var loadTemplates = function (names, callback) {
+// var loadTemplates = function (names, callback) {
 
-    var that = this;
+//     var that = this;
 
-    var loadTemplate = function (index) {
-        var name = names[index];
-        //console.log('Loading template: ' + name);
-        $.get('templates/' + name + '.html', function (data) {
-            that.templates[name] = data;
-            index++;
-            if (index < names.length) {
-                loadTemplate(index);
-            } else {
-                callback();
-            }
-        });
-    }
+//     var loadTemplate = function (index) {
+//         var name = names[index];
+//         //console.log('Loading template: ' + name);
+//         $.get('templates/' + name + '.html', function (data) {
+//             that.templates[name] = data;
+//             index++;
+//             if (index < names.length) {
+//                 loadTemplate(index);
+//             } else {
+//                 callback();
+//             }
+//         });
+//     }
 
-    loadTemplate(0);
-};
+//     loadTemplate(0);
+// };
 
 var AppRouter = Backbone.Router.extend({
     routes: {
@@ -27,15 +27,42 @@ var AppRouter = Backbone.Router.extend({
         "user/profile": "viewUserProfile",
     }
 });
-var HomeView = Backbone.View.extend({
+
+var MovieModel = Backbone.Model.extend({
+    initialize: function() {
+    }
+})
+
+
+var movies = [];
+movies.push(new MovieModel({title: "Hey there"}));
+movies.push(new MovieModel({title: "Hello"}));
+movies.push(new MovieModel({title: "heyheyhey"}));
+
+var MovieItemView = Backbone.View.extend({
+    initialize: function() {
+        this.render();
+    },
+    render: function() {
+        var template = _.template( $("#movieListItem").html(), {movie: this.model} );
+        this.$el.html(template);
+    }
+})
+
+var MovieListView = Backbone.View.extend({
     initialize: function() {
         this.render();
     },
     render: function() {
         // Compile the template using underscore
-        var template = _.template( $("#searchTemplate").html(), {} );
+        var template = _.template( $("#movieListTemplate").html(), {} );
             // Load the compiled HTML into the Backbone "el"
             this.$el.html( template );
+            for (var i=0;i<movies.length;i++){
+                console.log(movies[i]);
+                var movieItemView = new MovieItemView({model:movies[i]});
+                this.$el.append(movieItemView.el);
+            }
         }
     });
 
@@ -43,11 +70,10 @@ var HomeView = Backbone.View.extend({
     // Initiate the router
     var app_router = new AppRouter;
 
-    app_router.on('route:defaultRoute', loadTemplates(['search_template'],function(actions) {
-        var homeView = new HomeView({ el: $("#search_container") });
-        homeView.render();
-    }
-    ));
+    app_router.on('route:defaultRoute', function(actions) {
+        var movieListView = new MovieListView({ el: $("#list_container") });
+        movieListView.render();
+    });
     app_router.on('route:viewMovieDetails', function(id) {
 
     });
