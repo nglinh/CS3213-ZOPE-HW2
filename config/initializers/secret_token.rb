016de47c9c1b646a514af886ad99c3e96991ed31
Sai <jiangyaoxuan@gmail.com>
2013-09-11 15:32:29 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cs3213HW2::Application.config.secret_key_base = 'a7feffc0e3e354493b80306229e5d9e033e485a1612031da647bf140e9738905c5f250dcce4411de54069f9675e3935c3a629fac4162f45a51879c5b75d16336'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cs3213HW2::Application.config.session_store :cookie_store, key: '_cs3213HW2_session'
