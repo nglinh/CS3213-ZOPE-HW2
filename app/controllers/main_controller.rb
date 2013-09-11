af9d776a5b071265976221eb3333222cdd5fc765
Sai <jiangyaoxuan@gmail.com>
2013-09-11 15:45:41 +0800

    change callback

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,7 +7,7 @@ require 'net/http'
 PROVIDER = 'https://cs3213.herokuapp.com'
 APP_ID = '38313a31bcc76429957ba97fdd2b9404'
 APP_SECRET = 'fd9338dab2c23e99bde73b649458a11a'
-CALL_BACK_URL = 'http://localhost:3000/callback'
+CALL_BACK_URL = "#{request.host}/callback"
 
 def index
 	redirect_to "#{PROVIDER}/oauth/new?client_id=#{APP_ID}&redirect_uri=#{CALL_BACK_URL}"
