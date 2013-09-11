64c2419486941c786205585e0503db5455f3d761
Liu Tuo <liutuo098@gmail.com>
2013-09-11 23:42:07 +0800

    movie page with movie details and reviews displayed

--- js/zope.js
+++ js/zope.js
@@ -43,6 +43,38 @@ var Movie = Backbone.Model.extend({
     }
 });
 
+var Review = Backbone.Model.extend({
+    defaults: {
+        id: 'NA',
+        movie_id: 'NA',
+        comment: 'NA',
+        score: 'NA',
+        updated_at: 'NA',
+        user: {
+            id: 'NA',
+            username: 'NA',
+        }
+    },
+    urlRoot: "http://cs3213.herokuapp.com/movies/",
+    url: function() {
+        var base = this.urlRoot || (this.collection && this.collection.url) || "/";
+        return base  + (this.id) + ".json/reviews.json";
+    },
+});
+
+var Reviews = Backbone.Collection.extend({
+    initialize: function(options) {
+    this.id = options.id;
+  },
+    model: Review,
+    urlRoot: "http://cs3213.herokuapp.com/movies/",
+    url: function() {
+        var base = this.urlRoot || (this.collection && this.collection.url) || "/";
+        return base  + (this.id) + "/reviews.json";
+    }
+    
+});
+
 // To be used in the furute development
 // var movie = new Movie({id:movie_id});
 // movie.fetch(); // fetch model from DB with id = 1
@@ -105,6 +137,37 @@ var MovieListView = Backbone.View.extend({
     }});
 
 
+var MovieDetails = Backbone.View.extend({
+    el:'.movie_info_container',
+    render: function (id){
+        var self = this;
+        movie = new Movie({id:id});
+        movie.fetch({
+            success: function(movie) {
+                var template = _.template( $("#movie_info_template").html(), {movie: movie} );
+                self.$el.html(template);
+                console.log(movie.toJSON());
+            }
+        });
+    }
+});
+
+var ReviewList = Backbone.View.extend({
+    el:'.reviews_container',
+    render : function(id) {
+        var self = this;
+        reviews = new Reviews({id:id});
+        reviews.fetch({
+            success: function(reviews) {
+                console.log(reviews.toJSON());
+                var review_template = _.template($("#review_template").html(), {reviews: reviews.models});
+                self.$el.html(review_template);
+            }
+        });
+    }
+});
+
+
     // Initiate the router
     var app_router = new AppRouter;
 
@@ -112,6 +175,10 @@ var MovieListView = Backbone.View.extend({
         var movieListView = new MovieListView({ el: $("#list_container") });
     });
     app_router.on('route:viewMovieDetails', function(id) {
+        var movieDetails = new MovieDetails();
+        movieDetails.render(id);
+        var reviewList = new ReviewList();
+        reviewList.render(id);
 
     });
     app_router.on('route:createNewMovie', function() {
