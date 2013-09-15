b057cdd1ba3251dfb8f6c66896d9bc9701e1953c
Liu Tuo <archer@Lius-MacBook-Pro.local>
2013-09-15 23:06:56 +0800

    push again

--- app/assets/javascripts/zope.js
+++ app/assets/javascripts/zope.js
@@ -286,6 +286,7 @@ var MoviePage = Backbone.View.extend({
 var ReviewView = Backbone.View.extend ({
     initialize: function(option) {
         this.model = option.review;
+        this.el = option.el;
     },
     render: function() {
         var review_template = _.template($("#review_template").html(), {review: this.model, login_user:getUser()});
@@ -295,7 +296,7 @@ var ReviewView = Backbone.View.extend ({
         'click .delete-review' : 'deleteReview'
     },
     deleteReview: function () {
-        alert(model.get("id"));
+        alert(this.model.get("id"));
     }
 });
 
@@ -365,16 +366,14 @@ function getUser() {
         username: window.user_email
     };
 }
-function deleteReview() {
-    alert("delete");
-}
 
 function renderReviews(reviews) {
     for (var i =0;i< reviews.length;i++) {
         var review = reviews.at(i);
-        console.log("Review is: " );
-        console.log(review);
-        var reviewView = new ReviewView({el:$(".container"+review.get("id")), review:review});
+        var elid = "#"+review.get("id");
+        $("#list_container").append('<div class="productView'+review.id+'">');
+        var reviewView = new ReviewView({el: $('.productView'+review.id), review:review});
+        $("#list_container").append('</div>');
         reviewView.render();
     }
 }
